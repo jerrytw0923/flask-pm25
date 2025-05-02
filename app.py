6ddacb2cae6050f968ad9ca6b66d9e604830a14f
jerry <jerry@gmail.com>
2025-05-02 20:06:01 +0800

    完成後端取得site邏輯

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 import pandas as pd
-import pymysql
-from pm25 import get_pm25_data_from_mysql, update_db
+from pm25 import get_pm25_data_from_mysql, update_db, get_pm25_data_by_site
 import json
+
 
 app = Flask(__name__)
 
 
-@app.route("/filter", methods=["POST"])
-def filter_data():
-    # args=>form
-    county = request.form.get("county")
-    columns, datas = get_pm25_data_from_mysql()
-    df = pd.DataFrame(datas, columns=columns)
-    # 取得特定縣市的資料
-    df1 = df.groupby("county").get_group(county).groupby("site")["pm25"].mean()
-    print(df1)
-    return {"county": county}
+@app.route("/pm25-data-site")
+def pm25_data_by_site():
+    county = request.args.get("county")
+    site = request.args.get("site")
+
+    if not county or not site:
+        result = json.dumps({"error": "縣市跟站點名稱不能為空!"}, ensure_ascii=False)
+    else:
+        columns, datas = get_pm25_data_by_site(county, site)
+        df = pd.DataFrame(datas, columns=columns)
+        # 轉換字串時間格式
+        date = df["datacreationdate"].apply(lambda x: x.strftime("%Y-%m-%d %H"))
+        data = {
+            "county": county,
+            "site": site,
+            "x_data": date.to_list(),
+            "y_data": df["pm25"].to_list(),
+        }
+
+        result = json.dumps(data, ensure_ascii=False)
+
+    return result
 
 
 # 更新資料庫
@@ -65,54 +77,6 @@ def index():
         x_data=x_data,
         y_data=y_data,
     )
-
-
-@app.route("/books")
-def books_page():
-    books = [
-        {
-            "name": "Python book",
-            "price": 299,
-            "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
-        },
-        {
-            "name": "Java book",
-            "price": 399,
-            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
-        },
-        {
-            "name": "C# book",
-            "price": 499,
-            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
-        },
-    ]
-
-    books = []
-    if books:
-        for book in books:
-            print(book["name"])
-            print(book["price"])
-            print(book["image_url"])
-    else:
-        print("販售完畢，目前無書籍!")
-
-    username = "jerry"
-    nowtime = datetime.now().strftime("%Y-%m-%d")
-    print(username, nowtime)
-    return render_template("books.html", name=username, now=nowtime, books=books)
-
-
-@app.route("/bmi")
-def get_bmi():
-    # args=>GET
-    height = eval(request.args.get("height"))
-    weight = eval(request.args.get("weight"))
-
-    print(height, weight)
-
-    bmi = round(weight / (height / 100) ** 2, 2)
-
-    return render_template("bmi.html", **locals())
 
 
 if __name__ == "__main__":
