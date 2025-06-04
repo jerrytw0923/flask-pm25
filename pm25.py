3a4bbffe0dc01024b24c582489f2fc5d1fd55043
jerry <jerry@gmail.com>
2025-06-04 20:16:44 +0800

    發布前的資料準備

--- pm25.py
+++ pm25.py
@@ -79,11 +79,21 @@ def update_db():
     return row_count, message
 
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+
 def open_db():
     conn = None
     try:
         conn = pymysql.connect(
-            host="127.0.0.1", port=3306, user="root", passwd="12345678", db="demo"
+            host=os.environ.get("DB_HOST"),
+            port=int(os.environ.get("DB_PORT")),
+            user=os.environ.get("DB_USER"),
+            passwd=os.environ.get("DB_PASSWORD"),
+            db=os.environ.get("DB_NAME"),
         )
     except Exception as e:
         print("資料庫開啟失敗", e)
